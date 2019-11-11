742cf5d585ae778feb719bd75aac990d29ee4118
BADAWESEBASTIENSEBASTIEN <57428309+BADAWESEBASTIENSEBASTIEN@users.noreply.github.com>
2019-11-11 13:54:06 +0100

    Create Class Controleur.py
    Mon code python pour le compte du TP en programmation orientée objet du groupe 15.

---
+++ Class Controleur.py
@@ -0,0 +1,21 @@
+#coding: utf-8
+
+class Controleur:
+    def __init__(self, soldeCaisse, operation):
+        self.soldeCaisse = soldeCaisse
+        self.operation = operation
+    def verifier(self):
+        if self.soldeCaisse == self.operation:
+            print("Tout va pour le mieu")
+        else :
+            print("La somme des opérations journalières ne cadre pas avec le solde en caisse(problème)")
+
+
+print("Une operation est l'ensemble des transactions journalière éffectuer dans une banque ")
+
+c1 = Controleur(2000000, 2000000)
+c1.verifier()
+
+c2 = Controleur(10000000, 500000)
+c2.verifier()
+print("A reverifier en cas de problème.........")
